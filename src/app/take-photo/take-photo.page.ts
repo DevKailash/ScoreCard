731d18fface7c24bddbd6e8b27da8d8cf7015331
kailushan4 <kailashinfotech4@gmail.com>
2020-07-30 16:53:36 +0530

    bug fixes

--- src/app/take-photo/take-photo.page.ts
+++ src/app/take-photo/take-photo.page.ts
@@ -9,28 +9,28 @@ import { File} from '@ionic-native/file/ngx'
 })
 export class TakePhotoPage implements OnInit {
   imageUrl:any;
+  storedDetails:any;
   // storedPhoto:any;
   constructor(private camera: Camera, private file: File) { }
 
   ngOnInit() {
+    this.storedDetails = JSON.parse(window.localStorage.getItem("storedImg"));
+    this.imageUrl = (<any>window).Ionic.WebView.convertFileSrc(this.storedDetails.url+this.storedDetails.name); 
   }
 // After take picture close and reopen the application also will remove the already take picture.
   async accessCamera(){
     console.log("Accessing camera!");
-    const storedDetails:any = JSON.parse(window.localStorage.getItem("storedImg"));
-    console.log("path:",storedDetails);
-    alert(JSON.stringify(storedDetails));
-    if(storedDetails){
-      this.deleteImg(storedDetails.url,storedDetails.name);
+    if(this.storedDetails){
+      this.deleteImg(this.storedDetails.url,this.storedDetails.name);
     }else{
       this.openCamera();
     }    
   }
   async openCamera(){
-    alert("openCamera");
+    // alert("openCamera");
     this.imageUrl = '';
     const options: CameraOptions = {
-      quality: 100,
+      quality: 50,
       destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
@@ -42,21 +42,20 @@ export class TakePhotoPage implements OnInit {
     const tempImgPath = tempImage.substr(0, tempImage.lastIndexOf('/') + 1);
 
     const dataDirectoryPath = this.file.dataDirectory;
-
-    await this.file.copyFile(tempImgPath, tempFilename, 
-      dataDirectoryPath, tempFilename);
-
-    // this.storedPhoto = dataDirectoryPath + tempFilename;
+    // moveDir(path, dirName, newPath, newDirName)
+    await this.file.moveDir(tempImgPath, tempFilename, dataDirectoryPath, tempFilename);
     let storedPhoto = {
       url: dataDirectoryPath,
       name: tempFilename
     }
+    this.imageUrl = (<any>window).Ionic.WebView.convertFileSrc(dataDirectoryPath+tempFilename);
     window.localStorage.setItem("storedImg",JSON.stringify(storedPhoto));
 
   }
   deleteImg(filepath, fileName) {
-    // remove img from application catch;
+    // Removing img from application catch;
     this.file.removeFile(filepath, fileName).then(()=>{
+      alert("image deleted");
       this.openCamera();
     });
   }
