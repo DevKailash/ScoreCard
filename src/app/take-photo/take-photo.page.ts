de60c542e26440ed925935ebef148480d98b2364
kailushan4 <kailashinfotech4@gmail.com>
2020-07-30 15:52:36 +0530

    bug fixes

--- src/app/take-photo/take-photo.page.ts
+++ src/app/take-photo/take-photo.page.ts
@@ -9,7 +9,7 @@ import { File} from '@ionic-native/file/ngx'
 })
 export class TakePhotoPage implements OnInit {
   imageUrl:any;
-  storedPhoto:any;
+  // storedPhoto:any;
   constructor(private camera: Camera, private file: File) { }
 
   ngOnInit() {
@@ -17,7 +17,9 @@ export class TakePhotoPage implements OnInit {
 // After take picture close and reopen the application also will remove the already take picture.
   async accessCamera(){
     console.log("Accessing camera!");
-    const storedDetails:any = JSON.stringify(window.localStorage.getItem("storedImg"));
+    const storedDetails:any = JSON.parse(window.localStorage.getItem("storedImg"));
+    console.log("path:",storedDetails);
+    alert(JSON.stringify(storedDetails));
     if(storedDetails){
       this.deleteImg(storedDetails.url,storedDetails.name);
     }else{
@@ -25,17 +27,16 @@ export class TakePhotoPage implements OnInit {
     }    
   }
   async openCamera(){
+    alert("openCamera");
     this.imageUrl = '';
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
-      sourceType: this.camera.PictureSourceType.CAMERA,
-      targetWidth: 800,
-      targetHeight: 800,
     }
     const tempImage = await this.camera.getPicture(options);
+    this.imageUrl = tempImage;
     const tempFilename = tempImage.substr(tempImage.lastIndexOf('/') + 1);
     console.log(tempFilename);
     const tempImgPath = tempImage.substr(0, tempImage.lastIndexOf('/') + 1);
@@ -45,7 +46,7 @@ export class TakePhotoPage implements OnInit {
     await this.file.copyFile(tempImgPath, tempFilename, 
       dataDirectoryPath, tempFilename);
 
-    this.storedPhoto = dataDirectoryPath + tempFilename;
+    // this.storedPhoto = dataDirectoryPath + tempFilename;
     let storedPhoto = {
       url: dataDirectoryPath,
       name: tempFilename
